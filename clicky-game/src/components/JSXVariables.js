8450fa9c833a026abc253ff34cb81249636cb12d
JeromeHD <jaymagolan@gmail.com>
2019-03-26 17:56:49 -0400

    update

---
+++ clicky-game/src/components/JSXVariables.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function JSXVariables() {
+  return (
+    <div>
+      <header> Hello</header>
+      <div className="main-container">
+        <div className="container">
+          <div className="jumbotron">
+            {/* <h1>Hi! My name is ({name})</h1>
+          <h2>My name has ({name.length}) letters</h2>
+          <h2>My name without any vowels is: ({stripVowels(name)})</h2>
+          <h2>I think React ({thoughts})</h2> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default JSXVariables;
